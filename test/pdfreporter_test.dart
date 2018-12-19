910a0dcb7eb584b56c962fcb00589843ca9c6fa8
icosa2050 <icosa2050@gmail.com>
2018-12-19 23:34:00 +0100

    comment out for analyzer

--- test/pdfreporter_test.dart
+++ test/pdfreporter_test.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:pdfreporter/pdfreporter.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+//    final calculator = Calculator();
+    //   expect(calculator.addOne(2), 3);
+    //  expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    //expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
